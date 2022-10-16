90403bbff0d163cc6bf61e493d683abf2f8404ce
Fernando <a52hepof@uco.es>
2022-10-16 16:43:16 +0200

    simplifying project

--- start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#python3 /sqlite/sqlite_create.py
-#export PYTHONPATH=/app
-#echo $PYTHONPATH
-#gunicorn --workers=4 --threads=2 -b 0.0.0.0:80 main:app --timeout 0 --reload
-uvicorn main:app --host 0.0.0.0 --port 10012
